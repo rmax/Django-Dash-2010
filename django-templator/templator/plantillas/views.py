10f08d6fcd350324f1cf7c904a260fffb4e37d7a
Rolando Espinoza La fuente <darkrho@gmail.com>
2010-08-15 02:19:55 -0400

    simplified views to use template uuid and context Sign-Off: Rolando Espinoza La fuente

--- django-templator/templator/plantillas/views.py
+++ django-templator/templator/plantillas/views.py
@@ -4,9 +4,10 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
 from templator.plantillas import loader
-from templator.plantillas.models import Collection
+from templator.plantillas.models import TemplateContext
 from templator.plantillas.utils import (get_context_from_request,
                                         template_to_response,
+                                        get_object_or_none,
                                         JsonResponse)
 
 def validate_context(request):
@@ -29,25 +30,15 @@ def validate_context(request):
 
     return JsonResponse(data)
 
-def anon_template_render(request):
-    context = get_context_from_request(request)
+def template_render(request, uuid, path):
+    context = get_context(uuid)
+    with loader.set_context(template_uuid=uuid):
+        return template_to_response(path, context, request=request)
 
-    template_name = context.pop('__name__', '__name__')
-    template_content = context.pop('__content__', '')
-
-    if template_content:
-        with loader.set_context(template_content=template_content):
-            return template_to_response(template_name, context, request=request)
+def get_context(uuid):
+    obj = get_object_or_none(TemplateContext, group_uuid=uuid)
+    if obj and obj.context:
+        # context should contain a valid string
+        return ast.literal_eval(obj.context)
     else:
-        return redirect('/')
-
-def template_render(request, collection_name, template_name):
-    # TODO: auth?
-    collection = get_object_or_404(Collection, name=collection_name)
-    if not template_name:
-        raise Http404
-
-    # store collection in loader's thread local context
-    context = get_context_from_request(request)
-    with loader.set_context(collection=collection):
-        return template_to_response(template_name, context, request=request)
+        return {}
