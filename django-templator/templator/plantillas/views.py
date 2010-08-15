002aa174b8e126a8e86f298028bed8ef49ebb78a
Rolando Espinoza La fuente <darkrho@gmail.com>
2010-08-15 00:30:34 -0400

    added view to validate context through ajax call Sign-Off: Rolando Espinoza La fuente

--- django-templator/templator/plantillas/views.py
+++ django-templator/templator/plantillas/views.py
@@ -1,10 +1,33 @@
+import ast
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 
 from templator.plantillas import loader
 from templator.plantillas.models import Collection
 from templator.plantillas.utils import (get_context_from_request,
-                                        template_to_response)
+                                        template_to_response,
+                                        JsonResponse)
+
+def validate_context(request):
+    context = request.POST.get('context')
+    data = {'status': 'error'}
+    if context:
+        try:
+            ast.literal_eval(context)
+            data['status'] = 'ok'
+        except SyntaxError as e:
+            label = 'Syntax error: '
+            msg = '<pre>%s%s<br/>%s</pre>'
+            data['message'] = msg % (label, e.text,
+                                     "&nbsp;"*(e.offset-1+len(label))+"^")
+        except ValueError:
+            data['message'] = ("Invalid string. Only strings, numbers, lists "
+                               "and dicts supported")
+    else:
+        data['message'] = "context can't be empty"
+
+    return JsonResponse(data)
 
 def anon_template_render(request):
     context = get_context_from_request(request)
